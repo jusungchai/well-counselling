11f2c5490478c2de26f477614a811823c2f7c96b
JS Chai <chunsaa@gmail.com>
2020-03-26 12:49:13 -0400

    change image url for security

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -4,7 +4,7 @@ import '../styling/ImageCarousel.css'
 
 export default function ImageCarousel() {
   const slideImages = [
-    'http://wellcounselling.com/wp-content/themes/mesmerize/assets/images/home_page_header.jpg',
+    'https://content.linkedin.com/content/dam/me/learning/blog/2016/november/Work-Meeting-Small.jpg',
     'https://secureservercdn.net/198.71.233.204/5xi.ab1.myftpupload.com/wp-content/uploads/2018/03/qtq80-iBjOm3.jpeg',
     'https://secureservercdn.net/198.71.233.204/5xi.ab1.myftpupload.com/wp-content/uploads/2018/03/qtq50-Q7QAAZ.jpeg'
   ];
