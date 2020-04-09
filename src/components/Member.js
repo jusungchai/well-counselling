1905f16c98474d6022f2bd842fc263d116d20415
Jay <chunsaa@gmail.com>
2020-04-08 22:01:55 -0400

    fix props

--- src/components/Member.js
+++ src/components/Member.js
@@ -3,11 +3,10 @@ import '../styling/Member.css'
 
 export default function Member(props) {
   return (
-    <div className="member-container">
-      <img className="member-avatar" alt="member" src={props.data.avatar} />
+    <div className="member-container" onClick={() => console.log(props.data)} >
+      <img className="member-avatar" alt="member" src="https://secureservercdn.net/198.71.233.204/5xi.ab1.myftpupload.com/wp-content/uploads/2018/08/WanSoo-Chai.jpeg" />
       <div className="member-avatar-info">
-        <h3>{props.data.firstName} {props.data.lastName}</h3>
-        <p>{props.data.title}</p>
+        <h3>{props.data.firstname} {props.data.lastname}</h3>
       </div>
     </div>
   )
