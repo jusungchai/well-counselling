405c46428e8e60c0e1924531bc8b8fb35ed53cdf
Jay <chunsaa@gmail.com>
2020-04-10 01:30:49 -0400

    update profile form to support avatar upload

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import '../styling/Profile.css'
 
 export default function Profile() {
   const [bio, setBio] = useState({
+    avatar: "",
     info: "",
     jobTitle: "",
     specialField: "",
     certificate: "",
     experience: "",
-    degree: ""
+    degree: "",
+    blog: "",
+    avatarData: null
   })
 
   const [show, setShow] = useState({
@@ -20,7 +22,9 @@ export default function Profile() {
     specialField: "none",
     certificate: "none",
     experience: "none",
-    degree: "none"
+    degree: "none",
+    avatar: "none",
+    blog: "none"
   })
 
   useEffect(() => {
@@ -28,14 +32,15 @@ export default function Profile() {
       .then(res => {
         if (res.data.data) {
           console.log(res.data.data)
-          const { info, jobTitle, specialField, certificate, experience, degree } = res.data.data
+          const { info, jobTitle, specialField, certificate, experience, degree, blog } = res.data.data
           setBio({
             info,
             jobTitle,
             specialField,
             certificate,
             experience,
-            degree
+            degree,
+            blog
           })
         }
       })
@@ -55,10 +60,15 @@ export default function Profile() {
         return show.experience === "none" ? setShow({ ...show, experience: "block" }) : setShow({ ...show, experience: "none" })
       case "degree":
         return show.degree === "none" ? setShow({ ...show, degree: "block" }) : setShow({ ...show, degree: "none" })
+      case "avatar":
+        return show.avatar === "none" ? setShow({ ...show, avatar: "block" }) : setShow({ ...show, avatar: "none" })
+      case "blog":
+        return show.blog === "none" ? setShow({ ...show, blog: "block" }) : setShow({ ...show, blog: "none" })
       default:
         console.log("default")
     }
   }
+
   const handleChange = (e, type) => {
     switch (type) {
       case "info":
@@ -73,46 +83,105 @@ export default function Profile() {
         return setBio({ ...bio, experience: e.target.value })
       case "degree":
         return setBio({ ...bio, degree: e.target.value })
+      case "blog":
+        return setBio({ ...bio, blog: e.target.value })
+      case "avatar":
+        const avatarData = e.target.files[0];
+        if ((avatarData.type === "image/jpeg" || avatarData.type === "image/png") && (avatarData.size <= 2621440)) {
+          const formData = new FormData()          
+          formData.append('avatar', avatarData)          
+          setBio({ ...bio, avatar: e.target.value, avatarData: formData })
+          alert("ok")
+        } else {
+          setBio({ ...bio, avatar: "", avatarData: null })
+          alert("Image must be JPG/PNG and less than 2MB")
+        }
+        console.log(e.target.value)
+        break;
       default:
         console.log("default")
     }
   }
   const handleSubmit = (e) => {
     e.preventDefault()
+    console.log(bio.avatarData)
     axios.post('/profile', bio)
-    .then(res => alert(res.data))
+      .then(res => alert(res.data))
+  }
+
+  const placeHolder = {
+    info: "Name: Dr. John Doe\nResidence: Canada\nPhone: +1-416-123-1234\nEmail: john.doe@gmail.com\nCommunication: English/Korean\netc...",
+    jobTitle: "Title: CEO\nCompany Info\netc...",
+    specialField: "일반상담\n결혼과 가족 상담\netc...",
+    certificate: "Pastoral Counsellor (Bachelors)\nCertified Clinical Counsellor (Doctors)\netc...",
+    experience: "Marriage Counselling for 50+ clients - 2019\netc...",
+    degree: "학사/Bachelor of Art - Seoul University (2019)\n기독교상담학학사/Bachelor of Christian Counselling - Seoul University (Current)\netc...",
+    blog: "https://naver.com"
   }
 
   return (
     <div id="profile-container">
       <div className="main profile container">
-        <Form name="profileForm" className="profile form" onSubmit={(e) => handleSubmit(e)} >
-          <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label onClick={() => handleShow("info")}>INFO</Form.Label>
-            <Form.Control name="info" value={bio.info} placeholder="asdf&#10;qwe" as="textarea" rows="8" style={{ display: show.info }} onChange={(e) => handleChange(e, "info")} />
-          </Form.Group>
-          <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label onClick={() => handleShow("jobTitle")}>현제직위명</Form.Label>
-            <Form.Control name="jobTitle" value={bio.jobTitle} placeholder="asdf&#10;qwe" as="textarea" rows="8" style={{ display: show.jobTitle }} onChange={(e) => handleChange(e, "jobTitle")} />
-          </Form.Group>
-          <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label onClick={() => handleShow("specialField")}>전문상담/코칭분야</Form.Label>
-            <Form.Control name="specialField" value={bio.specialField} placeholder="asdf&#10;qwe" as="textarea" rows="8" style={{ display: show.specialField }} onChange={(e) => handleChange(e, "specialField")} />
-          </Form.Group>
-          <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label onClick={() => handleShow("certificate")}>전문자격증</Form.Label>
-            <Form.Control name="certificate" value={bio.certificate} placeholder="asdf&#10;qwe" as="textarea" rows="8" style={{ display: show.certificate }} onChange={(e) => handleChange(e, "certificate")} />
-          </Form.Group>
-          <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label onClick={() => handleShow("experience")}>상담/코칭 경력</Form.Label>
-            <Form.Control name="experience" value={bio.experience} placeholder="asdf&#10;qwe" as="textarea" rows="8" style={{ display: show.experience }} onChange={(e) => handleChange(e, "experience")} />
-          </Form.Group>
-          <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label onClick={() => handleShow("degree")}>EDUCATIONAL BACKGROUND DEGREE</Form.Label>
-            <Form.Control name="degree" value={bio.degree} placeholder="asdf&#10;qwe" as="textarea" rows="8" style={{ display: show.degree }} onChange={(e) => handleChange(e, "degree")} />
-          </Form.Group>
-          <Button type="submit">PUBLISH</Button>
-        </Form>
+        <form name="profileForm" className="profile form" onSubmit={(e) => handleSubmit(e)} >
+          <div className="profileContainer">
+            <Button className="btn btn-info" style={{ width: "100%" }} onClick={() => handleShow("avatar")}>PROFILE IMAGE</Button>
+            <div className="profileEditor" style={{ display: show.avatar }}>
+              <label style={{ color: "red", fontSize: "small" }}>(ONLY ADD TO CHANGE CURRENT FILE)</label>
+              <input className="form-control-file" id="exampleFormControlFile1" value={bio.avatar || ""} type='file' accept='image/jpeg, image/png' onChange={(e) => handleChange(e, "avatar")} />
+            </div>
+          </div>
+
+          <div className="profileContainer">
+            <Button className="btn btn-info" style={{ width: "100%" }} onClick={() => handleShow("info")}>INFO</Button>
+            <div className="profileEditor" style={{ display: show.info }}>
+              <textarea name="info" value={bio.info} placeholder={placeHolder.info} as="textarea" rows="8" onChange={(e) => handleChange(e, "info")} />
+            </div>
+          </div>
+
+          <div className="profileContainer">
+            <Button className="btn btn-info" style={{ width: "100%" }} onClick={() => handleShow("jobTitle")}>현제직위명</Button>
+            <div className="profileEditor" style={{ display: show.jobTitle }}>
+              <textarea name="jobTitle" value={bio.jobTitle} placeholder={placeHolder.jobTitle} as="textarea" rows="8" onChange={(e) => handleChange(e, "jobTitle")} />
+            </div>
+          </div>
+
+          <div className="profileContainer">
+            <Button className="btn btn-info" style={{ width: "100%" }} onClick={() => handleShow("specialField")}>전문상담/코칭분야</Button>
+            <div className="profileEditor" style={{ display: show.specialField }}>
+              <textarea name="specialField" value={bio.specialField} placeholder={placeHolder.specialField} as="textarea" rows="8" onChange={(e) => handleChange(e, "specialField")} />
+            </div>
+          </div>
+
+          <div className="profileContainer">
+            <Button className="btn btn-info" style={{ width: "100%" }} onClick={() => handleShow("certificate")}>전문 자격증</Button>
+            <div className="profileEditor" style={{ display: show.certificate }}>
+              <textarea name="certificate" value={bio.certificate} placeholder={placeHolder.certificate} as="textarea" rows="8" onChange={(e) => handleChange(e, "certificate")} />
+            </div>
+          </div>
+
+          <div className="profileContainer">
+            <Button className="btn btn-info" style={{ width: "100%" }} onClick={() => handleShow("experience")}>상담/코칭 경력</Button>
+            <div className="profileEditor" style={{ display: show.experience }}>
+              <textarea name="experience" value={bio.experience} placeholder={placeHolder.experience} as="textarea" rows="8" onChange={(e) => handleChange(e, "experience")} />
+            </div>
+          </div>
+
+          <div className="profileContainer">
+            <Button className="btn btn-info" style={{ width: "100%" }} onClick={() => handleShow("degree")}>EDUCATIONAL DEGREE</Button>
+            <div className="profileEditor" style={{ display: show.degree }}>
+              <textarea name="degree" value={bio.degree} placeholder={placeHolder.degree} as="textarea" rows="8" onChange={(e) => handleChange(e, "degree")} />
+            </div>
+          </div>
+
+          <div className="profileContainer">
+            <Button className="btn btn-info" style={{ width: "100%" }} onClick={() => handleShow("blog")}>BLOG</Button>
+            <div className="profileEditor" style={{ display: show.blog }}>
+              <input name="blog" value={bio.blog} placeholder={placeHolder.blog} type="text" rows="8" onChange={(e) => handleChange(e, "blog")} />
+            </div>
+          </div>
+
+          <Button style={{ width: "100%", marginTop: "2em" }} type="submit">PUBLISH</Button>
+        </form>
       </div>
     </div>
   )
