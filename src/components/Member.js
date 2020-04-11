850a366fb454af3047a99ffedfa40ee525dce37c
Jay <chunsaa@gmail.com>
2020-04-11 03:41:16 -0400

    create individual profile component

--- src/components/Member.js
+++ src/components/Member.js
@@ -3,8 +3,8 @@ import '../styling/Member.css'
 
 export default function Member(props) {
   return (
-    <div className="member-container" onClick={() => props.history.history.push(`/member/${props.data.id}`)} >
-      <img className="member-avatar" alt="member" src="https://secureservercdn.net/198.71.233.204/5xi.ab1.myftpupload.com/wp-content/uploads/2018/08/WanSoo-Chai.jpeg" />
+    <div className="member-container" onClick={() => props.history.history.push(`/member/${props.data.userid}`)} >
+      <img className="member-avatar" alt="member" src={props.data.avatarurl} />
       <div className="member-avatar-info">
         <h3>{props.data.firstname} {props.data.lastname}</h3>
       </div>
--- src/components/MemberProfile.js
+++ src/components/MemberProfile.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function MemberProfile({ match }) {
   const { params: { profileId } } = match;
-  return (
-    <div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <h1>YOLO</h1>
-      <h1>{profileId}</h1>
 
-    </div>
+  const [state, setState] = useState({
+    loading: true,
+    bio: ""
+  })
+
+  useEffect(() => {
+    axios.get(`/users/profile?id=${profileId}`)
+      .then(res => {
+        console.log(res.data[0])
+        if (res.data.length > 0) {
+          setState({ loading: false, bio: res.data[0].data })
+        } else {
+          setState({ loading: false, bio: { msg: "No Profile Found" } })
+        }
+      })
+      .catch(error => console.log(error))
+      console.log(state)
+  }, [])
+  
+  return state.loading ? null : (
+    state.bio.msg ?
+      <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>{state.bio.msg}</h1>
+      </div>
+      :
+      <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>YOLO</h1>
+        <h1>{profileId}</h1>
+        <p>{state.bio.info}</p>
+      </div>
   )
 }
